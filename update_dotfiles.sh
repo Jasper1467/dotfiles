1f79b4f2079e4bcff58292164d13d3e07836c92c
Jasper1467 <vectorcall@macesunrise.maskmy.id>
2024-06-02 04:30:38 +0200

    new file:   etc/mkinitcpio.conf 	new file:   etc/sysctl.d/99-sysctl.conf 	modified:   update_dotfiles.sh

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -7,8 +7,11 @@ sudo cp -r ~/.config/nvim/ ./nvim
 
 sudo mkdir ./etc
 sudo mkdir ./etc/pacman.d
+sudo mkdir ./etc/sysctl.d
 
+sudo cp -r /etc/mkinitcpio.conf ./etc/mkinitcpio.conf
 sudo cp -r /etc/pacman.conf ./etc/
 sudo cp -r /etc/pacman.d/mirrorlist ./etc/pacman.d/
 sudo cp -r /etc/makepkg.conf ./etc/
 sudo cp -r /etc/environment ./etc/
+sudo cp -r /etc/sysctl.d/99-sysctl.conf ./etc/sysctl.d/
