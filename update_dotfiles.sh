5f8bb89b63bb3ef0a4c990b7f990cf3ad0049687
Jasper1467 <vectorcall@macesunrise.maskmy.id>
2024-06-20 19:46:19 +0200

    modified:   .config/nvim/lazy-lock.json 	new file:   .config/nvim/lua/plugins/codeium.lua 	modified:   etc/makepkg.conf 	new file:   etc/makepkg.conf.d/fortran.conf 	new file:   etc/makepkg.conf.d/linux-config/.config 	new file:   etc/makepkg.conf.d/linux-lts.conf 	new file:   etc/makepkg.conf.d/napture.conf 	new file:   etc/makepkg.conf.d/rust.conf 	modified:   etc/pacman.d/mirrorlist 	modified:   update_dotfiles.sh

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -11,12 +11,19 @@ sudo cp -r ~/.config/helix/ ./.config/
 sudo mkdir ./etc
 sudo mkdir ./etc/pacman.d
 sudo mkdir ./etc/sysctl.d
+sudo mkdir ./etc/makepkg.conf.d
+sudo mkdir ./etc/makepkg.conf.d/linux-config
 
 sudo cp -r /etc/mkinitcpio.conf ./etc/mkinitcpio.conf
 sudo cp -r /etc/pacman.conf ./etc/
 sudo cp -r /etc/pacman.d/mirrorlist ./etc/pacman.d/
 sudo cp -r /etc/makepkg.conf ./etc/
+sudo cp -r /etc/makepkg.conf.d/rust.conf ./etc/makepkg.conf.d/
+sudo cp -r /etc/makepkg.conf.d/fortran.conf ./etc/makepkg.conf.d/
+sudo cp -r /etc/makepkg.conf.d/napture.conf ./etc/makepkg.conf.d/
+sudo cp -r /etc/makepkg.conf.d/linux-lts.conf ./etc/makepkg.conf.d/
+sudo cp -r /etc/makepkg.conf.d/linux-config/.config ./etc/makepkg.conf.d/linux-config/
 sudo cp -r /etc/sysctl.d/99-sysctl.conf ./etc/sysctl.d/
+sudo cp -r /etc/nanorc ./etc/nanorc
 sudo cp -r /etc/environment ./etc/
-sudo cp -r /etc/sysctl.d/99-sysctl.conf ./etc/sysctl.d/
 sudo cp -r /etc/nanorc ./etc/nanorc
